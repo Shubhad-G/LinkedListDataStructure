a83a9f13217f9ac86f252578a455cf17d6223e98
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-29 21:34:40 +0530

    add a print message

---
+++ LinkedListDataStructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("implementation of simple Single Linked List program");
+            Console.ReadLine();
+        }
+    }
+}
